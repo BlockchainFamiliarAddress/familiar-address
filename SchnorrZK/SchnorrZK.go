6991245fa9e3afe479397691293bb684a137f2a0
BlockchainFamiliarAddress <82842238+BlockchainFamiliarAddress@users.noreply.github.com>
2021-05-03 23:30:47 +0800

    Revert "finish user familiar key generate"

--- SchnorrZK/SchnorrZK.go
+++ SchnorrZK/SchnorrZK.go
@@ -5,24 +5,13 @@ import (
 	"familiar_address/miracl/core/BN254_FA"
 )
 
-func SZKProve(rng *core.RAND, group_type int, sk [32]byte, M []byte) (ret int, E [32]byte, S [32]byte) {
-	var nilRet [32]byte
-	if group_type != 1 && group_type != 2 {
-		return -1, nilRet, nilRet
-	}
-
+func SZKProve(rng *core.RAND, sk [32]byte, M []byte) (E [32]byte, S [32]byte) {
 	r := BN254_FA.NewBIGints(BN254_FA.CURVE_Order)
-	G1Base := BN254_FA.ECP_generator()
-	G2Base := BN254_FA.ECP2_generator()
+	G := BN254_FA.ECP2_generator()
 
 	rnd := BN254_FA.Randomnum(r, rng)
 	var rndPointBytes [65]byte
-
-	if group_type == 1 {
-		BN254_FA.G1mul(G1Base, rnd).ToBytes(rndPointBytes[:], true)
-	} else if group_type == 2 {
-		BN254_FA.G2mul(G2Base, rnd).ToBytes(rndPointBytes[:], true)
-	}
+	BN254_FA.G2mul(G, rnd).ToBytes(rndPointBytes[:], true)
 
 	H := core.NewHASH256()
 	H.Process_array(M)
@@ -40,31 +29,21 @@ func SZKProve(rng *core.RAND, group_type int, sk [32]byte, M []byte) (ret int, E
 	return
 }
 
-func SZKVerify(group_type int, pk [65]byte, M []byte, E [32]byte, S [32]byte) bool {
+func SZKVerify(pk [65]byte, M []byte, E [32]byte, S [32]byte) bool {
 	r := BN254_FA.NewBIGints(BN254_FA.CURVE_Order)
-	G1Base := BN254_FA.ECP_generator()
-	G2Base := BN254_FA.ECP2_generator()
+	G := BN254_FA.ECP2_generator()
 
 	s := BN254_FA.FromBytes(S[:])
 	e := BN254_FA.FromBytes(E[:])
+	pkPoint := BN254_FA.ECP2_fromBytes(pk[:])
 
+	sPoint := BN254_FA.G2mul(G, s)
+	minusE := BN254_FA.Modneg(e, r)
+
+	minusEPkPoint := BN254_FA.G2mul(pkPoint, minusE)
+	minusEPkPoint.Add(sPoint)
 	var epksPointBytes [65]byte
-
-	if group_type == 1 {
-		pkPoint := BN254_FA.ECP_fromBytes(pk[:])
-		sPoint := BN254_FA.G1mul(G1Base, s)
-		minusE := BN254_FA.Modneg(e, r)
-		minusEPkPoint := BN254_FA.G1mul(pkPoint, minusE)
-		minusEPkPoint.Add(sPoint)
-		minusEPkPoint.ToBytes(epksPointBytes[:], true)
-	} else if group_type == 2 {
-		pkPoint := BN254_FA.ECP2_fromBytes(pk[:])
-		sPoint := BN254_FA.G2mul(G2Base, s)
-		minusE := BN254_FA.Modneg(e, r)
-		minusEPkPoint := BN254_FA.G2mul(pkPoint, minusE)
-		minusEPkPoint.Add(sPoint)
-		minusEPkPoint.ToBytes(epksPointBytes[:], true)
-	}
+	minusEPkPoint.ToBytes(epksPointBytes[:], true)
 
 	H := core.NewHASH256()
 	H.Process_array(M)
